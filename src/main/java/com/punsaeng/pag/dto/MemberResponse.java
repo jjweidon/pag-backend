f70b01259800cd08d96ebbcd688240465f437426
정재웅 <jweidon0821@gmail.com>
2024-09-23 23:56:20 +0900

    Feat: 회원 DTO 추가

---
+++ src/main/java/com/punsaeng/pag/dto/MemberResponse.java
@@ -0,0 +1,12 @@
+package com.punsaeng.pag.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class MemberResponse {
+}
